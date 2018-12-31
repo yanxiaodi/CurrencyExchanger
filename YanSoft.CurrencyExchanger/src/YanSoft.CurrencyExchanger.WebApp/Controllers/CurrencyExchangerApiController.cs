3ae2e2723e9419dfd68028f1d7485cd881545338
Xiaodi Yan <yan_xiaodi@hotmail.com>
2018-12-31 17:01:42 +1300

    Add the web app.

---
+++ YanSoft.CurrencyExchanger/src/YanSoft.CurrencyExchanger.WebApp/Controllers/CurrencyExchangerApiController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YanSoft.CurrencyExchanger.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyExchangerApiController : ControllerBase
+    {
+        // GET: api/CurrencyExchangerApi
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CurrencyExchangerApi/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/CurrencyExchangerApi
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/CurrencyExchangerApi/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ YanSoft.CurrencyExchanger/src/YanSoft.CurrencyExchanger.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YanSoft.CurrencyExchanger.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
